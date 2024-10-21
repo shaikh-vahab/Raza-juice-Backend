8704745ccad84073687b89fc3c052385c319fdbb
shaikh-vahab <shaikhvahab97@gmail.com>
2024-10-21 14:42:49 +0530

    Added Cors

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import express from "express"
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from 'dotenv';
 import compression from "compression";
+import cors from 'cors'; // Import the cors package
 import RazaRouter from "./Routers/RazaRoute.js";
 import mongoose from "mongoose";
 
-
-dotenv.config()
+dotenv.config();
 const app = express();
 const port = process.env.PORT;
-const databaseUrl = process.env.DATABASEURL
+const databaseUrl = process.env.DATABASEURL;
 
- 
-mongoose.connect(databaseUrl).then((result) =>
-    console.log('Connected To Database Successfully')
-).catch((err) => {
-    console.log('Error Occured Connect Databse Time')
-})
+// Connect to MongoDB
+mongoose.connect(databaseUrl)
+    .then(() => console.log('Connected To Database Successfully'))
+    .catch((err) => console.log('Error Occurred Connecting to Database:', err));
 
-app.use(compression())
+// Use CORS middleware
+app.use(cors()); // Enable CORS for all routes
+
+app.use(compression());
 app.use(express.json());
-app.use('/Raza', RazaRouter)
+app.use('/Raza', RazaRouter);
 
-
+// Start the server
 app.listen(port, () => {
-    console.log(`server started at port ${port}`)
-}
-)
+    console.log(`Server started at port ${port}`);
+});
